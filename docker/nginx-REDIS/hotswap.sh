cc957ba6fb54457229cf2d5e7b7e8980975c5ab8
Tycho Yacub <tychoyacub@gmail.com>
2019-03-13 15:35:44 -0700

    Incorperated Extra Credit

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sed -i "s/^    server.*/    server $1:6379;/" /etc/nginx/nginx.conf
-/usr/sbin/nginx -s reload
+OldLine="$(grep "    server " etc/nginx/nginx.conf)"
+NewLine="    server $1:6379;"
+if [ "$NewLine" != "$OldLine" ]
+then
+	sed -i "s/^    server.*/$NewLine/" /etc/nginx/nginx.conf
+	/usr/sbin/nginx -s reload
+else
+	echo "Warning: Server given is already in use."
+fi
+
